d863f325ce5a446fe9514f47916cf50663e265a9
israel-sahar <37867238+israel-sahar@users.noreply.github.com>
2019-05-28 15:32:54 +0300

    Update script.js

--- triviapage/script.js
+++ triviapage/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    $('#b2').click(function () {
+        window.location.replace("../jokespage/jokespage.html");
+        
+    })
     var btn = document.getElementById('b1');
     btn.addEventListener('click', nextitem)
     b2.style.display = 'none'
