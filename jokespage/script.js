055478df2c11821f48e6bdb56bd0145a28183654
RaveElkabetz <48412301+RaveElkabetz@users.noreply.github.com>
2019-05-30 15:47:00 +0300

    Update script.js

---
+++ jokespage/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+var urlStr1 = 'http://api.icndb.com/jokes/random?firstName=';
+var urlStr2 = '&lastName=' ;
+$(localStorage.setItem("name","rave"))
+var fullName = $(localStorage.getItem("name"));
+var nameArr = fullName.selector.split(" ") ;
+var firstName = nameArr[0];
+var lastName = nameArr[1];
+if(lastName == undefined){
+    lastName = 'Banana';
+}
+var fullUrl = urlStr1.concat(firstName).concat(urlStr2).concat(lastName);
+
+//ajax
+$.ajax({
+    url: fullUrl,
+    type: "get",
+    success: function(data){
+        $(".lead").html(
+            data.value.joke
+        );
+    }, 
+    error: function(err){
+        console.log(err);
+    }
+});
+
+
+
+});
